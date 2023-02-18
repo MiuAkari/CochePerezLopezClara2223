46fab8f21bdd25e13fcd8337dcd4bd40f95dc1ef
clara <clara@DESKTOP-UJKBT41>
2023-02-18 20:04:44 +0100

    Modificación de la clase Main PerezLopezClara2122

--- CocheClaraGit/src/coche/Main.java
+++ CocheClaraGit/src/coche/Main.java
@@ -12,15 +12,14 @@ public class Main {
         int stockActual;
         
         miCoche = new Coche("Opel",12000,500);
-        try 
-        {
-            System.out.println("Venta de Coches");
-            miCoche.vender(300);
-        } catch (Exception e)
-        {
-            System.out.print("Fallo al vender");
-        }
+        operatoriaVentaPerezLopezClara2223(miCoche);        
+        operato0riaCompraPerezLopezClara2223(miCoche);
         
+        stockActual = miCoche.obtenerStock();
+        System.out.println("El stock actual es"+ stockActual );
+    }
+
+    public static void operato0riaCompraPerezLopezClara2223(Coche miCoche) {
         try
         {
             System.out.println("Compra de Coches");
@@ -29,8 +28,17 @@ public class Main {
         {
             System.out.print("Fallo al ingresar");
         }
-        stockActual = miCoche.obtenerStock();
-        System.out.println("El stock actual es"+ stockActual );
+    }
+
+    public static void operatoriaVentaPerezLopezClara2223(Coche miCoche) {
+        try
+        {
+            System.out.println("Venta de Coches");
+            miCoche.vender(300);
+        } catch (Exception e)
+        {
+            System.out.print("Fallo al vender");
+        }
     }
 
 }
